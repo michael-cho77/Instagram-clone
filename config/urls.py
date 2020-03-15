4719c40f6a4101962561e1c0657490e753f5397b
Youngchan <michael_cho77@naver.com>
2020-03-16 00:05:39 +0900

    Post (Main index)
    .

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,15 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.shortcuts import redirect
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
+    path('post/', include('post.urls', namespace='post')),
+    path('', lambda r: redirect('post:post_list'), name='root')
+    
 ]
 
 
