b1c66907966cac0325c86e26b213dbdfde1d0ce9
Youngchan <michael_cho77@naver.com>
2020-04-29 20:00:17 +0900

    Messages

---
+++ accounts/mixins.py
@@ -0,0 +1,33 @@
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, _("Can't go there"))
+        return redirect("core:home")
+
+
+# Using View only loggedOut User
+class LoggedOutOnlyView(UserPassesTestMixin):
+
+    permission_denied_message = "Page not found"
+
+    # return값이 True 인 경우는 오직 인증(로그인)받지 않은 유저만
+    def test_func(self):
+        return not self.request.user.is_authenticated
+
+    def handle_no_permission(self):
+        return redirect("core:home")
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+    login_url = reverse_lazy("users:login")
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.contrib.auth import logout as django_logout
-from .forms import SignupForm, LoginForm
-from .models import Profile, Follow
+from . import forms, models, mixins
+from django.contrib import messages
 
 import json
 from django.http import HttpResponse
@@ -10,22 +10,31 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
 
+class signup(mixins.LoggedOutOnlyView):
+    pass
+
 def signup(request):
+    if request.user.is_authenticated:
+        messages.error(request, "잘못된 접근경로입니다.")
+        return redirect(reverse("post:post_list"))
     if request.method == 'POST':
-        form = SignupForm(request.POST, request.FILES)
+        form = forms.SignupForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             return redirect('accounts:login')
     else:
-        form = SignupForm()
+        form = forms.SignupForm()
     return render(request, 'accounts/signup.html',{
         'form': form,
     }) 
 
 
 def login_check(request):
+    if request.user.is_authenticated:
+        messages.error(request, "잘못된 접근경로입니다.")
+        return redirect(reverse("post:post_list"))
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = forms.LoginForm(request.POST)
         name = request.POST.get('username')
         pwd = request.POST.get('password')
 
@@ -37,7 +46,7 @@ def login_check(request):
         else:
             return render(request, 'accounts/login_fail.html')
     else:
-        form = LoginForm()
+        form = forms.LoginForm()
         return render(request, 'accounts/login.html', {"form":form})
 
 
@@ -50,10 +59,13 @@ def logout(request):
 @login_required
 @require_POST
 def follow(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "잘못된 접근경로입니다.")
+        return redirect(reverse("post:post_list"))
     from_user = request.user.profile
     pk = request.POST.get('pk')
-    to_user = get_object_or_404(Profile, pk=pk)
-    follow, created = Follow.objects.get_or_create(from_user=from_user, to_user=to_user)
+    to_user = get_object_or_404(models.Profile, pk=pk)
+    follow, created = models.Follow.objects.get_or_create(from_user=from_user, to_user=to_user)
     
     if created:
         message = '팔로우 시작!'
